efa882176b15253e63febc6bbd08ce62192f2cbd
yuriysurzhikov <yuriisurzhykov@gmail.com>
2022-11-29 22:47:56 -0800

    Cleaned up resources usage

--- app/src/main/java/com/yuriisurzhykov/pointdetector/presentation/points/details/WorkingHoursListAdapter.kt
+++ app/src/main/java/com/yuriisurzhykov/pointdetector/presentation/points/details/WorkingHoursListAdapter.kt
@@ -1,17 +1,6 @@
 package com.yuriisurzhykov.pointdetector.presentation.points.details
 
-import androidx.recyclerview.widget.DiffUtil
 import com.yuriisurzhykov.pointdetector.presentation.list.BaseViewHolderTypeManager
-import com.yuriisurzhykov.pointsdetector.uicomponents.list.AbstractRecyclerAdapter
-import com.yuriisurzhykov.pointsdetector.uicomponents.list.HolderDiffUtilsCallback
-import com.yuriisurzhykov.pointsdetector.uicomponents.list.ViewHolderItem
+import com.yuriisurzhykov.pointsdetector.uicomponents.list.BaseRecyclerViewAdapter
 
-class WorkingHoursListAdapter :
-    AbstractRecyclerAdapter<ViewHolderItem, WorkingHoursViewHolder>(BaseViewHolderTypeManager()) {
-
-    override fun getDiffCallback(
-        oldItems: List<ViewHolderItem>, newItems: List<ViewHolderItem>
-    ): DiffUtil.Callback {
-        return HolderDiffUtilsCallback(oldItems, newItems)
-    }
-}
+class WorkingHoursListAdapter : BaseRecyclerViewAdapter(BaseViewHolderTypeManager())
