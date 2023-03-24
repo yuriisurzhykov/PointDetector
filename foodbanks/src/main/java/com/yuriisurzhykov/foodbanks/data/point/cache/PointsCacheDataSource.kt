91bbfe68b64578d8172dec0a35295e1efb6c66b4
yuriysurzhikov <yuriisurzhykov@gmail.com>
2023-03-24 16:25:28 -0700

    Created points cache data source

--- foodbanks/src/main/java/com/yuriisurzhykov/foodbanks/data/point/cache/PointsCacheDataSource.kt
+++ foodbanks/src/main/java/com/yuriisurzhykov/foodbanks/data/point/cache/PointsCacheDataSource.kt
@@ -1,7 +1,22 @@
 package com.yuriisurzhykov.foodbanks.data.point.cache
 
+import com.yuriisurzhykov.foodbanks.data.prefs.SelectedCityPreference
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 
 interface PointsCacheDataSource {
-    fun points(): Flow<PointCache>
+
+    suspend fun points(): Flow<PointCache>
+
+    abstract class Abstract(
+        private val cityPreference: SelectedCityPreference, private val pointsDao: PointsDao
+    ) : PointsCacheDataSource {
+
+        override suspend fun points() = pointsDao.pointsByCity(cityPreference.getPrefValue())
+    }
+
+    class Base @Inject constructor(
+        cityPreference: SelectedCityPreference,
+        pointsDao: PointsDao
+    ) : Abstract(cityPreference, pointsDao)
 }
