453eff2968f2681bb0ba70621987b240c659f54e
yuriysurzhikov <yuriisurzhykov@gmail.com>
2023-03-30 18:26:02 -0700

    Created use case for cities

--- foodbanks/src/main/java/com/yuriisurzhykov/foodbanks/domain/city/City.kt
+++ foodbanks/src/main/java/com/yuriisurzhykov/foodbanks/domain/city/City.kt
@@ -1,4 +1,16 @@
 package com.yuriisurzhykov.foodbanks.domain.city
 
-class City {
+import com.yuriisurzhykov.foodbanks.core.domain.Select
+import com.yuriisurzhykov.foodbanks.data.prefs.PreferenceDataSource
+
+class City(
+    private val cityName: String,
+    private val cityCode: String,
+    private val country: String,
+    private val region: String
+) : Select<String> {
+
+    override suspend fun select(preference: PreferenceDataSource<String>) {
+        preference.savePref(cityCode)
+    }
 }
