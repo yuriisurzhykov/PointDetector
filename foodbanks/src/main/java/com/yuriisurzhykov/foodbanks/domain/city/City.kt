978f87fc4077afb5cec655c62ec139a21d182000
yuriysurzhikov <yuriisurzhykov@gmail.com>
2023-04-03 21:14:34 -0700

    Fixed logic with using mappers

--- foodbanks/src/main/java/com/yuriisurzhykov/foodbanks/domain/city/City.kt
+++ foodbanks/src/main/java/com/yuriisurzhykov/foodbanks/domain/city/City.kt
@@ -1,16 +1,38 @@
 package com.yuriisurzhykov.foodbanks.domain.city
 
-import com.yuriisurzhykov.foodbanks.core.domain.Select
+import com.yuriisurzhykov.foodbanks.core.presentation.Dispatchers
 import com.yuriisurzhykov.foodbanks.data.prefs.PreferenceDataSource
+import kotlinx.coroutines.CoroutineScope
 
-class City(
-    private val cityName: String,
-    private val cityCode: String,
-    private val country: String,
-    private val region: String
-) : Select<String> {
+interface City {
 
-    override suspend fun select(preference: PreferenceDataSource<String>) {
-        preference.savePref(cityCode)
+    interface Mapper<T> {
+
+        fun map(cityName: String, cityCode: String, country: String, region: String): T
+
+        class SaveToPreferences(
+            private val preference: PreferenceDataSource<String>,
+            private val dispatchers: Dispatchers,
+            private val coroutineScope: CoroutineScope
+        ) : Mapper<Unit> {
+
+            override fun map(cityName: String, cityCode: String, country: String, region: String) {
+                dispatchers.launchBackground(coroutineScope) {
+                    preference.savePref(cityCode)
+                }
+            }
+        }
+    }
+
+    fun <T> map(mapper: Mapper<T>): T
+
+    class Base(
+        private val cityName: String,
+        private val cityCode: String,
+        private val country: String,
+        private val region: String
+    ) : City {
+
+        override fun <T> map(mapper: Mapper<T>): T = mapper.map(cityName, cityCode, country, region)
     }
 }
