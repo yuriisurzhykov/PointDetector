e73c8c4b220aadb7e874df1736d777fdcaa880e1
Yurii Surzhykov <44873047+yuriisurzhykov@users.noreply.github.com>
2024-02-15 15:22:06 -0800

    Refactor vibration service creation

--- app/src/main/java/com/yuriisurzhykov/pointdetector/PointDetectorApp.kt
+++ app/src/main/java/com/yuriisurzhykov/pointdetector/PointDetectorApp.kt
@@ -4,5 +4,4 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class PointDetectorApp : Application() {
-}
+class PointDetectorApp : Application()
--- app/src/main/java/com/yuriisurzhykov/pointdetector/presentation/vibration/VibrationService.kt
+++ app/src/main/java/com/yuriisurzhykov/pointdetector/presentation/vibration/VibrationService.kt
@@ -5,12 +5,14 @@ import android.os.Build
 import com.yuriisurzhykov.pointdetector.presentation.IVibrationService
 
 class VibrationService : IVibrationService {
-    override fun vibrate(context: Context) {
-        val service = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+    private val service by lazy {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             VibratorServiceApi30()
         } else {
             VibratorServiceApi29()
         }
+    }
+    override fun vibrate(context: Context) {
         service.vibrate(context)
     }
 }
